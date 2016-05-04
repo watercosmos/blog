328044881c8c03a84007681e03d9f302c254925d
liuhanyu <liuhanyu@meituan.com>
2016-05-04 19:45:39 +0800

    整合 log4j2

--- src/main/java/com/lhy/blog/controller/UserController.java
+++ src/main/java/com/lhy/blog/controller/UserController.java
@@ -2,6 +2,8 @@ package com.lhy.blog.controller;
 
 import com.lhy.blog.domain.User;
 import com.lhy.blog.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,26 +18,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     UserService userService;
 
     @RequestMapping(method = RequestMethod.GET)
     public User getUserById(@RequestParam("userId") Integer userId) {
+        LOGGER.info("getUserById: userId = {}", userId);
         return userService.getUserById(userId);
     }
 
     @RequestMapping(method = RequestMethod.POST)
     public int insertUser(@RequestBody User user) {
+        LOGGER.info("insertUser: user = {}", user);
         return userService.insertUser(user);
     }
 
     @RequestMapping(method = RequestMethod.PUT)
     public int updateUser(@RequestBody User user) {
+        LOGGER.info("updateUser: user = {}", user);
         return userService.updateUser(user);
     }
 
     @RequestMapping(method = RequestMethod.DELETE)
     public int deleteUserById(@RequestParam("userId") Integer userId) {
+        LOGGER.info("deleteUserById: userId = {}", userId);
         return userService.deleteUser(userId);
     }
 
