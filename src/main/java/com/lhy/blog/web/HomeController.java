ef50393828f29451eb84611c6d5f8419616d71fd
liuhanyu <liuhanyu@meituan.com>
2016-05-10 17:26:37 +0800

    添加 thymeleaf 支持

---
+++ src/main/java/com/lhy/blog/web/HomeController.java
@@ -0,0 +1,19 @@
+package com.lhy.blog.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by lhy on 16/5/10.
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("welcome", "Welcome to my blog.");
+        return "home";
+    }
+
+}
